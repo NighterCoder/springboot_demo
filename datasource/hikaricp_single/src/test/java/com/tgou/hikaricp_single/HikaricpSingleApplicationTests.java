d6b757a75103ae07842e6419f9829665d5c602b2
lingzhan <m18340872285@163.com>
2020-07-24 15:39:09 +0800

    first commit

---
+++ datasource/hikaricp_single/src/test/java/com/tgou/hikaricp_single/HikaricpSingleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgou.hikaricp_single;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikaricpSingleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
